198f599657a101f04fcaaa82f30fb4feb28e4635
Juma Antony <jumaantony840@gmail.com>
2023-03-03 14:13:08 +0300

    creating project files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { Navbar } from './components/Navbar'
+import { Routes } from './components/Routes'
+import { Footer} from './components/Footer'
 
 const App = () => {
+    
   return (
     <div>
       <h1> App </h1>
