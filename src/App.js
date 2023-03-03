b38a7bf42fa5f396ac9d423f0324baab641d99fb
Juma Antony <jumaantony840@gmail.com>
2023-03-03 15:34:05 +0300

    created a navbar with dark/light theme toggler

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { useState } from 'react'
 
-import { Navbar } from './components/Navbar'
-import { Routes } from './components/Routes'
-import { Footer} from './components/Footer'
+import Navbar from './components/Navbar';
+import Routes from './components/Routes';
+import Footer  from './components/Footer';
 
 const App = () => {
-    
-  return (
-    <div>
-      <h1> App </h1>
-    </div>
-  )
+    const [darkTheme, setDarkTheme ] = useState(false);
+
+    return (
+        <div className={darkTheme ? 'dark' : ''}>
+            <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen'>
+                <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+                <Routes />
+                <Footer />
+            </div>
+        </div>
+    )
 }
 
-export default App
+export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {Link }from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({darkTheme, setDarkTheme}) => {
   return (
-    <div>
-      <h1> Navbar </h1>
+    <div className='p-5 pb-0 flex flex-wrap sm:justify-between justify-center border-b dark:border-gray-700 border-gray-200'>
+      <div className='flex justify-between items-center space-x-5 w-screen'>
+        <Link to="/">
+            <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900">
+                Googgle 🔎
+            </p>
+             
+        </Link>
+        <button type='button' onClick={ () => setDarkTheme(!darkTheme) } 
+        className='text-sm dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg font-bold'>
+            {darkTheme ? 'Light 💡' : 'Dark 🌙'}
+        </button>
+      </div>
     </div>
   )
 }
