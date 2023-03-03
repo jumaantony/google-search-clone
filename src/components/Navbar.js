67798aceec64c65d85abaca7266e0af4dabac404
Juma Antony <jumaantony840@gmail.com>
2023-03-03 15:41:05 +0300

    created search file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Link }from 'react-router-dom';
 
+import Search from './Search'
+
 const Navbar = ({darkTheme, setDarkTheme}) => {
   return (
     <div className='p-5 pb-0 flex flex-wrap sm:justify-between justify-center border-b dark:border-gray-700 border-gray-200'>
@@ -16,6 +18,7 @@ const Navbar = ({darkTheme, setDarkTheme}) => {
             {darkTheme ? 'Light 💡' : 'Dark 🌙'}
         </button>
       </div>
+      <Search />
     </div>
   )
 }
