94ab5369730a410c964ca9fe3fc05ee1d0b9ae85
Juma Antony <jumaantony840@gmail.com>
2023-03-07 15:09:38 +0300

    tweaked routes

--- src/components/MyRoutes.js
+++ src/components/MyRoutes.js
@@ -9,7 +9,7 @@ const MyRoutes = () => {
       <Routes>
         <Route path="/" element={<Navigate to="/search" />} />
         <Route path="/search" element={<Results />} />
-        <Route path="/images" element={<Results />} />
+        {/* <Route path="/images" element={<Results />} /> */}
         {/* 
         <Route path="/news" element={<Results />} />
         <Route path="/videos" element={<Results />} /> */}
